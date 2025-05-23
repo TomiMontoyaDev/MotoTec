4e26406337461ff3372ad571065be272fe9a8ff9
TomiMontoya <tomasmontoyabuitrago@gmail.com>
2025-05-23 15:11:05 -0500

    fix: update import paths to absolute references for consistency

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Inicio from "./Inicio.jsx";
-import Proyeccion from "./Proyecciones.jsx";
+import "/App.css";
+import Inicio from "/Inicio.jsx";
+import Proyeccion from "/Proyecciones.jsx";
 export function App() {
   return (
     <BrowserRouter>
